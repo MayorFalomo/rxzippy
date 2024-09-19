0b81ff16b86a3d2c19da2f08b36b41aebfcc983f
mayor falomo <mayorfalomo@gmail.com>
2024-09-19 18:20:16 +0100

    updated bestFor

--- src/components/layout/medicalCourier/BestFor.tsx
+++ src/components/layout/medicalCourier/BestFor.tsx
@@ -1,11 +1,14 @@
 import doctArms from "@/assets/doctArms.webp";
-import TextMask from "@/Animation/TextMask";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { FaUserDoctor } from "react-icons/fa6";
+import { PiHospital } from 'react-icons/pi'
+import { ImLab } from 'react-icons/im';
+import { FaBriefcaseMedical } from "react-icons/fa";
+import FadeIn from "@/Animation/FadeIn";
 
 const BestFor = () => {
-  const Maps = ["Best for", "Pharmacies, Physicians", "Hospitals, Doctors"];
+  // const Maps = ["Best for", "Pharmacies, Physicians", "Hospitals, Doctors"];
   const [ref, inView] = useInView({
     threshold: 0.3,
   });
@@ -16,7 +19,7 @@ const BestFor = () => {
   });
 
   return (
-    <div className="h-[70vh] max-[870px]:h-auto flex items-center my-[50px] max-[870px]:my-[80px]  justify-center">
+    <div className="h-[80vh] max-[870px]:h-auto flex items-center my-[50px] max-[870px]:my-[80px]  justify-center">
       <div className="flex max-[870px]:flex-col-reverse items-center justify-between w-[85%] gap-[40px]">
         <div
           ref={(el) => {
@@ -25,29 +28,42 @@ const BestFor = () => {
           }}
           className="flex flex-col gap-4 "
         >
-          <div className="flex flex-col gap-4  ">
+          <div className="flex flex-col gap-4 ">
+
             <h2 className="block text-offGray text-[40px] max-[700px]:text-[35px] max-[480px]:text-[32px] font-hkGrotesk scroll-m-20 font-medium tracking-tight">
               Best For{" "}
             </h2>
-            <TextMask
-              inView={inView}
-              customStyles="flex w-full text-offGray text-[40px] max-[700px]:text-[35px] max-[480px]:text-[32px] font-hkGrotesk scroll-m-20 font-medium tracking-tight"
+            <FadeIn inView={inView} >
+            <div className=" flex items-center gap-[20px]" >
+            <div  className="flex items-center gap-3 text-offGray"
             >
-              <span>{<FaUserDoctor />} </span>
-              <span>Physicians </span>
-            </TextMask>
-            <h2 className="flex flex-col text-offGray text-[40px] max-[700px]:text-[35px] max-[480px]:text-[32px] font-hkGrotesk scroll-m-20 font-medium tracking-tight">
-              {/* {Maps.map((res, index) => (
-              <span key={index}>{res}</span>
-            ))} */}
-              {/* <span>Best for:</span>
-            <span>Doctor, hospitals, </span>
-            <span>Pharmacies</span> */}
-            </h2>
+              <span className=" bg-primary text-white p-3 max-[700px]:p-2 rounded-full text-[22px] max-[700px]:text-[35px] max-[480px]:text-[20px]" >{<FaBriefcaseMedical />} </span>
+              <p className=" w-full text-offGray  min-[700px]:text-[30px] max-[700px]:text-[25px] max-[480px]:text-[20px] font-hkGrotesk scroll-m-20 font-medium tracking-tight" >Pharmacies </p>
+            </div>
+            <div  className="flex items-center gap-3 text-offGray"
+            >
+              <span className=" bg-primary text-white p-3 max-[700px]:p-2 rounded-full text-[22px] max-[700px]:text-[35px] max-[480px]:text-[20px]" >{<PiHospital />} </span>
+              <p className=" w-full text-offGray  min-[700px]:text-[30px] max-[700px]:text-[25px] max-[480px]:text-[20px] font-hkGrotesk scroll-m-20 font-medium tracking-tight" >Hospitals </p>
+              </div>
+              </div>
+            </FadeIn>
+            <FadeIn inView={inView} >
+            <div className=" flex items-center justify-between gap-[20px]" >
+            <div  className="flex items-center gap-3 text-offGray"
+            >
+              <span className=" bg-primary text-white p-3 max-[700px]:p-2 rounded-full text-[22px] max-[700px]:text-[35px] max-[480px]:text-[20px]" >{<ImLab />} </span>
+              <p className=" w-full text-offGray min-[700px]:text-[30px] max-[700px]:text-[25px] max-[480px]:text-[20px] font-hkGrotesk scroll-m-20 font-medium tracking-tight" >Labs </p>
+            </div>
+            <div  className="flex items-center gap-3 text-offGray"
+            >
+              <span className=" bg-primary text-white p-3 max-[700px]:p-2 rounded-full text-[22px] max-[700px]:text-[35px] max-[480px]:text-[20px]" >{<FaUserDoctor />} </span>
+              <p className=" w-full text-offGray  min-[700px]:text-[30px] max-[700px]:text-[25px] max-[480px]:text-[20px] font-hkGrotesk scroll-m-20 font-medium tracking-tight" >Physician </p>
+                </div>
+                
+              </div>
+              </FadeIn>
           </div>
-          {/* <div>
-            <TrackYourPackageBtn />
-          </div> */}
+         
         </div>
         <div className="relative max-[1024px]:w-[400px] min-w-[400px] max-[570px]:w-[90%] max-[570px]:min-w-0">
           <motion.img
